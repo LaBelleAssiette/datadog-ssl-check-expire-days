ace87df7ed5808456bd3cf4619eb8a7fbf656a6b
Antoine Bolvy <antoine@labelleassiette.com>
2019-08-07 14:41:58 +0200

    Datadog agent v6 compat

--- ssl_check_expire_days.py
+++ ssl_check_expire_days.py
@@ -1,26 +1,38 @@
-# A rewrite of https://workshop.avatarnewyork.com/project/datadog-ssl-expires-check/
-# I prefer to test the site itself instead of the ssl cert file
-
 import time
 import datetime
-import subprocess
 import sys
-from checks import AgentCheck
+
+# the following try/except block will make the custom check compatible with any Agent version
+try:
+    # first, try to import the base class from old versions of the Agent...
+    from checks import AgentCheck
+    import subprocress
+
+    # polyfill for datadog v6 function
+    def get_subprocess_output(command, log):
+        completed = subprocess.run(command, capture_output=True)
+        return (completed.stdout, completed.stderr, completed.returncode)
+
+
+except ImportError:
+    # ...if the above failed, the check is running in Agent version 6 or later
+    from datadog_checks.checks import AgentCheck
+    from datadog_checks.utils.subprocess_output import get_subprocess_output
+
 
 class SSLCheckExpireDays(AgentCheck):
     def check(self, instance):
         metric = "ssl.expire_in_days"
         site = instance['site']
-        p = subprocess.Popen("echo | timeout 10 openssl s_client -showcerts -servername " + site + " -connect " + site + ":443 2>/dev/null | openssl x509 -noout -dates | grep notAfter | cut -f 2 -d\= | xargs -0 -I arg date -d arg \"+%s\"",stdout=subprocess.PIPE, shell=True)
-        (output, err) = p.communicate()
+        tag = "site:" + site # generate the tags
+        command = ["timeout", "10", "bash", "-c", "openssl s_client -showcerts -servername " + site + " -connect " + site + ":443 2>/dev/null | openssl x509 -noout -dates | grep notAfter | cut -f 2 -d\= | xargs -0 -I arg date -d arg '+%s'"];
+        (output, err, returncode) = get_subprocess_output(command, self.log, False)
         if output:
             output = output.rstrip("\n")
             d0 = int(time.time())
             d1 = int(output)
             delta = d1 - d0
-            days= delta/24/60/60 # convert the timestamp to days
-            tag="site:" + site # generate the tags
-#            print "metric: " + str(metric) + ", tag: " + str(tag) + ", days: " + str(days)
+            days = delta / 24 / 60 / 60 # convert the timestamp to days
             self.gauge(metric, int(days), tags=[tag])
         else:
-            self.gauge(metric, -1, tags=[site])
+            self.gauge(metric, -1, tags=[tag])
